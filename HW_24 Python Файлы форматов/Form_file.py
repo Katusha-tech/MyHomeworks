1a03464f0f760815252b1194dd1171d0c5196a71
Ekaterina <katusha158@rambler.ru>
2024-12-28 23:48:32 +0300

    Функции для работы с CSV-файлами

--- HW_24 Python Файлы форматов/Form_file.py
+++ HW_24 Python Файлы форматов/Form_file.py
@@ -1,7 +1,65 @@
+import csv
+import os
+
+os.system("cls")
 """
 Функция JSON (чтение, запись, добавление данных)
 """
 import json
+
+"""
+Функции для работы с CSV
+"""
+def read_csv(file_path, delimiter=';', encoding: str ='utf-8'):
+    """
+    Чтение данных из CSV-файла
+    Args:
+        file_path: путь к файлу
+        delimiter: разделитель полей в файле (по умолчанию `';'`)
+        encoding: кодировка файла
+    """
+    with open(file_path, "r", encoding=encoding) as file:
+        reader = csv.reader(file, delimiter=delimiter)
+        data = list(reader)
+    
+    return data
+
+
+def write_csv(data, file_path, delimiter=';', encoding: str = 'utf-8') -> None:
+    """
+    Запись данных в CSV-файл
+    Args:
+        data: данные для записи
+        file_path: путь к файлу
+        delimiter: разделитель полей в файле (по умолчанию `';'`)
+        encoding: кодировка файла 
+    """
+    with open(file_path, "w", encoding="utf-8-sig", newline='') as file:
+        writer = csv.writer(file, delimiter=delimiter, lineterminator="\n")
+        writer.writerows(data)
+
+
+def append_csv(data, file_path, delimiter=';', encoding: str ='utf-8') -> None:
+    """
+    Добавление данных в CSV - файл
+    Args:
+        data: данные для добавления
+        file_path: путь к файлу
+        delimiter: разделитель полей в файле (по умолчанию `';'`)
+        encoding: кодировка файла
+    """
+    with open(file_path, "a", encoding=encoding, newline='') as file:
+        writer = csv.writer(file, delimiter=delimiter, lineterminator="\n")
+        if isinstance(data[0], list): #если data - это список списков
+            writer.writerows(data) #добавление строк
+        else:    
+            writer.writerow(data) #добавление строки
+
+
+
+
+
+
 
 
 """
@@ -41,4 +99,6 @@ def append_txt(data, file_path, encoding: str = "utf-8") -> None:
     """
     with open(file_path, 'a', encoding=encoding) as file:
         file.write(data)
+
+       
 
