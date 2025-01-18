0d011b3b7b0c2e21adaf7c5d4e06945322c6521f
Ekaterina <katusha158@rambler.ru>
2025-01-18 21:53:17 +0300

    На функцию написала декоратор password_checker проверка пароля

--- HW_26 Декоратор/decor_1.py
+++ HW_26 Декоратор/decor_1.py
@@ -1,46 +1,43 @@
 from string import (
     ascii_lowercase, ascii_uppercase,
-    punctuation, digits)
+    punctuation)
+from typing import Callable
 
-
-def register_user(user_password:str = ""):
-    """
-    Функция, которая проверяет вводимый пароль на определенные критерии
-    :param user_password: str - пароль, который вводит пользователь
-    """
-    if not user_password:
-        return "Пароль не введен"
+def password_checker(func: Callable) -> Callable:
+    def wrapper(user_password: str):
+        """
+        Обертка для функции, проверяющая пароль перед выполнением основной логики
+        """
+        if not user_password:
+            return "Пароль не введен"
     
-    if len(user_password) < 8:
-        return "Слишком короткий пароль"
+        if len(user_password) < 8:
+            return "Слишком короткий пароль"
 
-    has_digit = False
-    has_upper = False
-    has_lower = False
-    has_symbol = False
+        has_digit = any(char.isdigit() for char in user_password)
+        has_upper = any(char in ascii_uppercase for char in user_password)
+        has_lower = any(char in ascii_lowercase for char in user_password)
+        has_symbol = any(char in punctuation for char in user_password)
 
-    for char in user_password:
-        if char.isdigit():
-            has_digit = True
-        elif char in ascii_uppercase:
-            has_upper = True
-        elif char in ascii_lowercase:
-            has_lower = True
-        elif char in punctuation:
-            has_symbol = True
+        if not has_digit:
+            return "Пароль должен содержать цифры"
+        if not has_upper:
+            return "Пароль должен содержать заглавную букву"
+        if not has_lower:
+            return "Пароль должен содержать строчную букву"
+        if not has_symbol:
+            return "Пароль должен содержать символы"
+        
+        return func(user_password)
+    return wrapper
 
-    if not has_digit:
-        return "Пароль должен содержать цифры"
-    if not has_upper:
-        return "Пароль должен содержать заглавную букву"
-    if not has_lower:
-        return "Пароль должен содержать строчную букву"
-    if not has_symbol:
-        return "Пароль должен содержать символы"
-    
+@password_checker
+def register_user(user_password: str):
     return "Успешная регистрация"
-
+    
 
 user_password = input("Введите пароль: ")
 result = register_user(user_password)
 print(result)
+
+
