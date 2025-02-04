c76a52ebe5f9b3ae964039bb292f8cbd09684b7c
Ekaterina <katusha158@rambler.ru>
2025-02-04 10:02:07 +0300

    Создание класса  и методов

---
+++ HW_28 Python Класс для TXT/classTXT.py
@@ -0,0 +1,24 @@
+class TxtFileHandler():
+    def read_file(filepath: str) -> str:
+        """
+        Чтение данных из TXT файла
+        param: filepath - путь файла
+        return: str - возврат строки
+        """
+        pass
+
+    def write_file(filepath: str, *data: str) -> None:
+        """
+        Запись данных в TXT файл
+        param: filepath - путь файла
+        data - данные
+        """
+        pass
+
+    def append_file(filepath: str, *data: str) -> None:
+        """
+        Дозаписывание данных в TXT файл
+        param: filepath -  путь
+        data - данные
+        """
+    def read_file
