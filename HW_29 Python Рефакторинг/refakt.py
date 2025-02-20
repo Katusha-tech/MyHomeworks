987e51fc9d074ad965b4d824124bd5a6244eb2a7
Ekaterina <katusha158@rambler.ru>
2025-02-20 09:35:12 +0300

    Добавление проверок

--- HW_29 Python Рефакторинг/refakt.py
+++ HW_29 Python Рефакторинг/refakt.py
@@ -36,11 +36,14 @@ class ImageCompressor:
 
         Returns:None
         """
+        if not input_path.lower().endswith(self.supported_formats):
+            raise ValueError("Неподдерживаемый формат файла")
+        
         with Image.open(input_path) as img:
             img.save(output_path, "HEIF", quality=self.__quality)
         print(f"Сжато: {input_path} -> {output_path}")
 
-    def process_directory(directory: str) -> None:
+    def process_directory(self, directory: str) -> None:
         """
         Обрабатывает все изображения в указанной директории и её поддиректориях.
         Args:
@@ -62,23 +65,27 @@ def main(input_path: str) -> None:
         input_path (str): Путь к файлу или директории для обработки.
     Returns:None
     """
-    input_path = input_path.strip('"')  # Удаляем кавычки, если они есть
-    compressor = ImageCompressor
-    
-    if os.path.exists(input_path):
-        if os.path.isfile(input_path):
+    try:
+        input_path = input_path.strip('"')  # Удаляем кавычки, если они есть
+        compressor = ImageCompressor(QUALITY)
+
+        if os.path.exists(input_path):
+            if os.path.isfile(input_path):
             # Если указан путь к файлу, обрабатываем только этот файл
-            print(f"Обрабатываем файл: {input_path}")
-            output_path = os.path.splitext(input_path)[0] + '.heic'
-            compressor.compress_image(input_path, output_path)
-        elif os.path.isdir(input_path):
+                print(f"Обрабатываем файл: {input_path}")
+                output_path = os.path.splitext(input_path)[0] + '.heic'
+                compressor.compress_image(input_path, output_path)
+            elif os.path.isdir(input_path):
             # Если указан путь к директории, обрабатываем все файлы в ней
-            print(f"Обрабатываем директорию: {input_path}")
-            compressor.process_directory(input_path)
+                print(f"Обрабатываем директорию: {input_path}")
+                compressor.process_directory(input_path)
             # Функция process_directory рекурсивно обойдет все поддиректории
             # и обработает все поддерживаемые изображения
-    else:
-        print("Указанный путь не существует")
+        else:
+            print("Указанный путь не существует")
+
+    except Exception as e:
+        print(f"Произошла ошибка: {e}")
 
 if __name__ == "__main__":
     user_input: str = input("Введите путь к файлу или директории: ")
